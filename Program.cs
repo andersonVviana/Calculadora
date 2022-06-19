cc639c52077566930f1c6e712297b4a0f45085ab
Anderson Viana <andy_vallada@yahoo.com.br>
2022-06-19 11:12:20 -0300

    Incluindo a Subtração

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
-﻿Console.Clear();
+﻿Subtracao();
 
-Console.WriteLine("Primeio Valor");
-float v1 = float.Parse(Console.ReadLine());
+static void Soma() {
 
-Console.WriteLine("Segundo Valor");
-float v2 = float.Parse(Console.ReadLine());
+    Console.Clear();
 
-Console.WriteLine("");
+    Console.WriteLine("Primeio Valor");
+    float v1 = float.Parse(Console.ReadLine());
 
-float resultado = v1 + v2;
-Console.WriteLine($"O Resultado da soma é {resultado}");
+    Console.WriteLine("Segundo Valor");
+    float v2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("");
+
+    float resultado = v1 + v2;
+    Console.WriteLine($"O Resultado da soma é {resultado}");
+}
+
+static void Subtracao() {
+    Console.Clear();
+
+    Console.WriteLine("Primeiro Valor: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Primeiro Valor: ");
+    float v2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("");
+
+    float resultado = v1-v2;
+    Console.WriteLine($"O Resultado da Subtração é {resultado}");
+    Console.ReadKey();
+}
