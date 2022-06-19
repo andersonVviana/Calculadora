1a9c5a616115e8bd5d0aec0328e5570e6adcb00c
Anderson Viana <andy_vallada@yahoo.com.br>
2022-06-19 10:59:10 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine("Primeio Valor");
+float v1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Segundo Valor");
+float v2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("");
+
+float resultado = v1 + v2;
+Console.WriteLine($"O Resultado da soma é {resultado}");
